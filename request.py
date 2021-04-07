ffa40811ba8dfd2a32fc2a3f333d011571246ffd
Alouhda <alsu.hudaiberdina@mail.ru>
2021-04-07 16:24:48 +0300

    loop

--- request.py
+++ request.py
@@ -1,8 +1,12 @@
 import requests
 import random
 from create_random_lst import ls_for
+import time
 
-res = requests.get("http://127.0.0.1:5000/")
+#res = requests.get("http://127.0.0.1:5000/")
 
-print(res.text)
-print(ls_for)
+#print(res)
+while True:
+    print("Send reques")
+    res = requests.post("http://127.0.0.1:5000/", {"lst": ls_for()})
+    time.sleep(3)
