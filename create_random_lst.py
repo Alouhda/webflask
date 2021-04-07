51794e17584136d0fa99b0f31153befbd2892bd8
Alouhda <alsu.hudaiberdina@mail.ru>
2021-04-07 16:23:04 +0300

    clear_funcion

--- create_random_lst.py
+++ create_random_lst.py
@@ -6,14 +6,13 @@ def ls_while():
     while len(numbers) != length:
         numbers.append(random.randint(10,100))
     return(numbers)
-print(ls_while())
 
 	
 def ls_for():
     numbers = list()
-    length = random.randrint(1,11)
+    length = random.randint(1,11)
     for i in range(length):
         numbers.append(random.randint(10,100))
     return(numbers)
-print(ls_while())
+
 	
