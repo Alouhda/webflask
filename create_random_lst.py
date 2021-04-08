97f7a75b129edef53d402a2f2470eead818a3a1f
tolstoy92 <33932807+tolstoy92@users.noreply.github.com>
2021-04-08 10:50:18 +0300

    Update create_random_lst.py

--- create_random_lst.py
+++ create_random_lst.py
@@ -5,7 +5,7 @@ def ls_while():
     length = random.randint(1,10)
     while len(numbers) != length:
         numbers.append(random.randint(10,100))
-    return(numbers)
+    return(numbers)  # вместо такой конструкции следует использовать `return numbers` 
 
 	
 def ls_for():
@@ -15,4 +15,13 @@ def ls_for():
         numbers.append(random.randint(10,100))
     return(numbers)
 
-	
+
+# в питоне есть очень полезная конструкция:
+# some_list = [element for element in range(100)] ## можно почитать тут: https://habr.com/ru/post/320288/
+# ниже - пример ее использования
+def get_rand_list():
+    low_val = 10
+    up_val = 100
+    rand_length = random.randint(1, 11)
+    numbers = [random.randint(low_val, up_val) for _ in range(rand_length)]
+    return numbers
