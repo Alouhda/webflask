3ef6aae431c57f5699d23562f319ee163cb91aad
tolstoy92 <33932807+tolstoy92@users.noreply.github.com>
2021-04-08 10:44:19 +0300

    Update request.py

--- request.py
+++ request.py
@@ -4,9 +4,14 @@ from create_random_lst import ls_for
 import time
 
 #res = requests.get("http://127.0.0.1:5000/")
+# ^ закомментированный лучше удалять, что бы не мазолил глаз и не заставлял гадать, зачем он нужен
+
 
 #print(res)
+# ^ комментарии лучше удалять
+
 while True:
     print("Send reques")
     res = requests.post("http://127.0.0.1:5000/", {"lst": ls_for()})
+    #                   ^ лучше задасть в начале скрипта как константу
     time.sleep(3)
