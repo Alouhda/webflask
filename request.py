83c03ca4f04c17d6dbe9886e5f404902a82b53d2
Alouhda <alsu.hudaiberdina@mail.ru>
2021-04-10 14:08:17 +0300

    correct loop

--- request.py
+++ request.py
@@ -1,12 +1,10 @@
 import requests
 import random
-from create_random_lst import ls_for
+from create_random_lst import get_rand_list
 import time
 
-#res = requests.get("http://127.0.0.1:5000/")
-
-#print(res)
+url = 'http://127.0.0.1:5000/'
 while True:
     print("Send reques")
-    res = requests.post("http://127.0.0.1:5000/", {"lst": ls_for()})
+    res = requests.post(url, {'lst': get_rand_list()})
     time.sleep(3)
