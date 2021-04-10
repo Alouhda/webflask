200969a2306bac53662137cb8abb084179e61d39
Alouhda <alsu.hudaiberdina@mail.ru>
2021-04-10 13:23:26 +0300

    post method

--- flask_app.py
+++ flask_app.py
@@ -5,11 +5,6 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-    
-
 @app.route('/', methods=["POST"])
 def req():
     data = request.form.to_dict(flat=False)
