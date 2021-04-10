c20d7486eaa3d5069f510c2b2d94cbd72f19fdcb
Alouhda <alsu.hudaiberdina@mail.ru>
2021-04-10 13:20:57 +0300

    list generator

--- create_random_lst.py
+++ create_random_lst.py
@@ -15,4 +15,9 @@ def ls_for():
         numbers.append(random.randint(10,100))
     return(numbers)
 
-	
+def get_rand_list():
+    low_val = 10
+    up_val = 100
+    rand_length = random.randint(1,10)
+    numbers = [random.randint(low_val, up_val) for _ in range(rand_length)]
+    return numbers
